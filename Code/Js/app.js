b8ff8abda05c60188bf656581f89f93b01ff0406
Jamie <31694@ma-web.nl>
2022-06-04 12:34:58 +0200

    New vid + Visueel concept

--- Code/Js/app.js
+++ Code/Js/app.js
@@ -1,4 +1,3 @@
-
 let overlay = document.querySelector(".menu_overlay");
 let open = document.querySelector(".fa-bars");
 let close = document.querySelector(".fa-times");
@@ -6,9 +5,11 @@ let close = document.querySelector(".fa-times");
 console.log(overlay, open, close);
 
 open.addEventListener("click", () => {
-  overlay.style.transform = "translateY(0)";
+overlay.style.transform = "translateY(0)";
 });
 
 close.addEventListener("click", () => {
-  overlay.style.transform = "translateY(-110%)";
+overlay.style.transform = "translateY(-110%)";
 });
+
+
